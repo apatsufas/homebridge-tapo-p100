c33a0235d4343dd233ba81ef438bb2f1303f7e5e
Andreas Zimnas <zimnasandreas@hotmail.com>
2021-02-24 21:19:42 +0200

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { API } from 'homebridge';
-declare const _default: (api: API) => void;
-/**
- * This method registers the platform with Homebridge
- */
-export = _default;
-//# sourceMappingURL=index.d.ts.map
