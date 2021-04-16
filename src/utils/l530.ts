1093da3bd0b64d2d9fe0ff5e13883391f542633f
Andreas Zimnas <zimnasandreas@hotmail.com>
2021-04-16 10:33:14 +0300

    fix for setting color

--- src/utils/l530.ts
+++ src/utils/l530.ts
@@ -63,6 +63,12 @@ export default class L530 extends L510E {
   }
 
   async setColor(hue:number, saturation:number):Promise<true>{
+    if(!hue){
+      hue = 0;
+    }
+    if(!saturation){
+      saturation = 0;
+    }
     const payload = '{'+
               '"method": "set_device_info",'+
               '"params": {'+
